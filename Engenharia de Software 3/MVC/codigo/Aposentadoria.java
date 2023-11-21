125e439b8f0c2a280713bb5bcbd3d313513f6797
Gabriel L <103595204+GabrAngelis@users.noreply.github.com>
2023-11-21 09:18:10 -0300

    Delete Engenharia de Software 3/MVC directory

--- Engenharia de Software 3/MVC/codigo/Aposentadoria.java
+++
@@ -1,3 +0,0 @@
-public interface Aposentadoria {
-    void emitirAposentadoria();
-}
--- Engenharia de Software 3/MVC/codigo/Subject.java
+++
@@ -1,6 +0,0 @@
-import java.util.List;
-
-public interface Subject {
-    void registerObserver(Observer obs);
-    void notifyObservers(List<String> encontrados);
-}
